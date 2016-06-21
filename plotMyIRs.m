bb53b5415de602c84c89038c9be43eb6512f597b
Spencer <spencer.jackson@iconfitness.com>
2016-06-21 16:53:15 -0600

    pretty well finalized

--- plotMyIRs.m
+++ plotMyIRs.m
@@ -2,10 +2,10 @@
 
 pkg load signal
 [y fs b] = wavread("before-1.wav");
-y = y/mean(abs(y));%normalize
+y = y/max(abs(y));%normalize
 l = length(y);
 [y2 fs b] = wavread("nopaper-1.wav");
-norm = mean(abs(y2));
+norm = max(abs(y2));
 y2 = y2/norm;
 l2 = length(y2);
 [y3 fs b] = wavread("wpaper-1.wav");
@@ -21,21 +21,6 @@ plotIREQ(y2,fs,'g');
 plotIREQ(y3,fs,'r');
 legend('untreated','no paper', 'paper')
 hold off
-
-
-if(0)
-%if(~dec)
-    semilogx( fs/l:fs/l:fs/2, 20*log10(abs(fft(y(:,1)))(1:ceil(l/2))),
-         fs/l2:fs/l2:fs/2, 20*log10(abs(fft(y2(:,1)))(1:ceil(l2/2))),
-         fs/l3:fs/l3:fs/2, 20*log10(abs(fft(y3(:,1)))(1:ceil(l3/2)))
-        )
-xlim([20 20000])
-title('Frequency response')
-xlabel('Hz')
-ylabel('dB')
-legend('untreated','no paper', 'paper')
-grid('on')
-end
 
 % smoothed version
 figure(2)
@@ -56,44 +41,11 @@ grid('on')
 
 %spectrogram
 figure(3)
-clf
-specgram(y(:,1),256,fs,hanning(256),256-32)
-title('Spectrogram')
+plotIRwaterfall(y,fs);
+title("Before")
 
-
-%waterfall plot
-%this plot is tuned for looking at room responses for resonances
-%figure(3)
-windowlen = 500;%ms
-rise = 100;%ms
-slices = 101;
-
-%y = y2;
-nfft = 2^ceil(log2(windowlen/1000*fs));
-lhs = rise/1000*fs;%samples of hanning rise time
-win = [hanning(2*lhs)(1:lhs); tukeywin(nfft,.25)(lhs+1:end)];%window has hanning rise and rest is .25 tukey
-step = 2^floor(log2(length(y)/slices));%time size of each slice of spectra
-overlap = nfft - step;
-[S, f, t] = specgram (y(:,1), nfft, fs, win,  overlap);%nfft - nfft/8);
-ff = f' * ones (1, length (t));
-tt = ones (length (f), 1) * t;
-Sdb = 20*log10(abs(S'));
-%TODO: may want to smooth it a bit 
-srf = waterfall (tt', ff'+.1, Sdb);%a minor offset is added to avoid 0 values on the log scale
-ax = get(srf,'parent');
-set(ax,'yscale','log');
-view([100,15,15])
-grid('on')
-shading('faceted')
-ylim([10 200])
-zmx = max(max(Sdb));
-zlim([zmx-60 1.1*zmx])
-xlabel('sec')
-ylabel('Hz')
-zlabel('dB')
-colormap('jet')
-
-
-figure(1)
+figure(4) 
+plotIRwaterfall(y3,fs);
+title("After")
 
 
